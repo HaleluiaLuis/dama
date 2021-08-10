129a29cd00df8939c36f2eadf64e835d748e24e6
Aleluia Luis <ledofuerte@gmail.com>
2021-08-10 21:32:12 +0100

    primeiro commit

---
+++ components/Subdivisao.jsx
@@ -0,0 +1,17 @@
+import styles from '../styles/subdivisao.module.css'
+
+export default function subdivisao(props) {
+    return (
+        <div
+            style={{
+                backgroundColor: props.preta ? "#000" : "#fff",
+            }} 
+            className={styles.subdivisao}>
+
+        </div>
+        
+    )
+       
+        
+    
+}  
